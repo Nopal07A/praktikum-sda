904ae037d34c778124a322fbbd907dc3dbfdcc97
Nopal07A <mnaufalal101@gmail.com>
2026-03-01 11:45:18 +0800

    sda pt 1

--- POPSTEST_1/nyoba.cpp
+++ POPSTEST_1/nyoba.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+
+    int A[8] = {1, 1, 2, 3, 5, 8, 13, 21};
+
+    int min = A[0];
+    int indexMin = 0;
+
+    for(int i = 1; i < 8; i++) {
+        if(A[i] < min) {
+            min = A[i];
+            indexMin = i;
+        }
+    }
+
+    cout << "Nilai minimum = " << min << endl;
+    cout << "Indeks minimum = " << indexMin << endl;
+
+    return 0;
+}
